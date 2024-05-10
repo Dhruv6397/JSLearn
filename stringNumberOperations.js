efba26019caa52f69aaa975a4bc694feac2379a6
dhruv6397 <vermadhruv6397@gmail.com>
2024-05-10 17:13:04 +0530

    String to number in javascript

---
+++ stringNumberOperations.js
@@ -0,0 +1,24 @@
+let value =3
+let negValue =-value
+console.log(value)//3
+console.log(negValue)//-3
+console.log(2-2)//0
+console.log(2*2)//4
+console.log(2**3)//8
+
+let str1 = "hello"
+let str2 = " Dhruv"
+console.log(str1+str2)//"hello Dhruv"
+console.log("1" + 2)//"12"
+console.log("1"+2+2)//"122"
+console.log(2+2+"2")//"42"
+
+console.log(+true)//1
+console.log(true + false + true)//2
+console.log(+"")//0
+
+let num1, num2, num3 = 6
+console.log(num1)//NaN
+console.log(num1+num2+num3)//NaN
+console.log(undefined +undefined)//NaN
+
